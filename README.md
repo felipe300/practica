396943970927c7c7753897993be9d8bdc1774ce9
Felipe300 <caracoloro@gmail.com>
2023-04-12 22:12:32 -0400

    feat: add search by name

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Basic api website
 - [API](https://digimon-api.vercel.app/)
 - [End Points](https://digimon-api.vercel.app/api/digimon)
 - [Github](https://github.com/felipe300/practica)
+
+## TODO
+- [x]: fix if user inser invalid number
+- [x]: add another event
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,42 @@
 const mainTitle = document.getElementById("mainTitle")
 const form = document.getElementById("digimonForm")
 
+function clearText () {
+	document.getElementById('digimonName').value = ""
+}
+
 form.addEventListener("submit", async (e) => {
 	e.preventDefault()
-	const digimonName = document.getElementById("digimonName").value
+	let digimonName = document.getElementById("digimonName").value
 
-	const url = `https://pokeapi.co/api/v2/pokemon/${digimonName.toLowerCase()}`
+	const url = `https://digimon-api.vercel.app/api/digimon`
 	const response = await fetch(url)
 	const digimon = await response.json()
+	// TODO: fix bad digimon name
+	const name = digimonName[0].toUpperCase() + digimonName.substring(1)
+
+	const getDigimon = digimon.filter((digi) => digi.name === name ? digi : '')
 
 	try {
 		let cardTitle = document.querySelector("#info-digimon .card-title")
-		cardTitle.innerHTML = (digimon.name).toUpperCase()
+		cardTitle.innerHTML = (getDigimon[0].name).toUpperCase()
+
+		let cardLevel = document.getElementById("level")
+		cardLevel.innerHTML = (getDigimon[0].level).toUpperCase()
 
 		let cardImage = document.querySelector("#info-digimon img")
-		cardImage.setAttribute("src", digimon.sprites.front_default)
+		cardImage.setAttribute("src", getDigimon[0].img)
 
-		let stats = document.getElementById("stats")
-		stats.innerHTML = ""
+		// let stats = document.getElementById("stats")
+		// stats.innerHTML = ""
 
-		let acc = ""
-		digimon.stats.forEach((stat) => {
-			let el = `<p>${stat.stat.name} => ${stat.base_stat}</p>`
-			acc += el
-		})
-		stats.innerHTML += acc
+		// let acc = ""
+		// digimon.stats.forEach((stat) => {
+		// 	let el = `<p>${stat.stat.name} => ${stat.base_stat}</p>`
+		// 	acc += el
+		// })
+		// stats.innerHTML += acc
+		clearText()
 	} catch (err) {
 		console.log(err)
 	}
